30691624de6ff44eb7fcde12349c444ebe2455c8
Pablo Cervigon <pablocervigon@gmail.com>
2025-02-08 14:03:28 +0100

    createPlaylist implemented getAllPlaylists implemented

--- playlist/js/playlist.js
+++ playlist/js/playlist.js
@@ -27,13 +27,21 @@ const musicCatalog = () => {
    * Adds a new playlist to the catalog.
    * @param {string} playlistName - The name of the new playlist.
    */
-  const createPlaylist = (playlistName) => {};
+  const createPlaylist = (playlistName) => {
+    const newPlaylist = {
+      name: playlistName,
+      songs: []
+    };
+    playlists = [...playlists, newPlaylist];
+  };
 
   /**
    * Gets all playlists in the catalog.
    * @returns {Playlist[]} The list of all playlists.
    */
-  const getAllPlaylists = () => {};
+  const getAllPlaylists = () => {
+    return playlists;
+  };
 
   /**
    * Removes a playlist from the catalog.
